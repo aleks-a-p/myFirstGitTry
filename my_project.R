837909cdd0287c21a332346d6bec98fe8d191bed
aleks-a-p <aleksandra.petelski@gmail.com>
2019-05-01 16:31:54 -0400

    add a comlex function

--- my_project.R
+++ my_project.R
@@ -2,4 +2,6 @@ data = iris
 plot(x = iris$Sepal.Length, y = iris$Sepal.Width)
 plot(x = iris$Petal.Length, y = iris$Petal.Width)
 hist(x = iris$Sepal.Length)
-
+complex_function <- function(input) { 
+  print("we are doing complex things here!")
+  }
