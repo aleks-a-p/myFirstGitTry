8ccd6e3fabb1d85694a682ca5e5cfd029beb4486
aleks-a-p <aleksandra.petelski@gmail.com>
2019-05-01 16:33:32 -0400

    second complex function

--- my_project.R
+++ my_project.R
@@ -2,6 +2,10 @@ data = iris
 plot(x = iris$Sepal.Length, y = iris$Sepal.Width)
 plot(x = iris$Petal.Length, y = iris$Petal.Width)
 hist(x = iris$Sepal.Length)
-complex_function <- function(input) { 
+complex_function1 <- function(input) { 
   print("we are doing complex things here!")
-  }
+}
+
+complex_function2 <- function(input) { 
+  print("we are doing complex things here!")
+}
